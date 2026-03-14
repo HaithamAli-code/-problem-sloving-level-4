5cf69aa66bf8349177588d9791956cda1cd5101e
haitham <haithamalibarhomah@gmail.com>
2026-03-14 18:21:33 +0300

    Update

---
+++ problem 2.cpp
@@ -0,0 +1,33 @@
+//
+#include<iostream>
+using namespace std;
+
+int ReadYear()
+{
+	int year;
+	cout << "Enter a year: ";
+	cin >> year;
+	return year;
+}
+
+bool IsLeapYear(int Year)
+{
+	if(Year % 400 == 0)
+		return true;
+	else if(Year % 100 == 0)
+		return false;
+	else if(Year % 4 == 0)
+		return true;
+	else
+		return false;
+}
+
+int main()
+{
+	int Year = ReadYear();
+
+	if (IsLeapYear(Year))
+		cout << "\nThe year " << Year << " is a leap year." << endl;
+	else
+		cout << "\nThe year " << Year << " is not a leap year." << endl;
+}
---
+++ problem 3.cpp
@@ -0,0 +1,35 @@
+// Leap Year(One Line Of Code)
+#include<iostream>
+using namespace std;
+
+int ReadYear()
+{
+	int year;
+	cout << "Enter a year: ";
+	cin >> year;
+	return year;
+}
+
+bool IsLeapYear(int Year)
+{
+	//if (Year % 400 == 0)
+	//	return true;
+	//else if (Year % 100 == 0)
+	//	return false;
+	//else if (Year % 4 == 0)
+	//	return true;
+	//else
+	//	return false;
+
+	return (Year % 4 == 0 || Year % 100 != 0) || (Year % 400 == 0);
+}
+
+int main()
+{
+	int Year = ReadYear();
+
+	if (IsLeapYear(Year))
+		cout << "\nThe year " << Year << " is a leap year." << endl;
+	else
+		cout << "\nThe year " << Year << " is not a leap year." << endl;
+}
